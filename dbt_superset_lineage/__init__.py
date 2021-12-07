6556134f795491cc941ebd98084a85a16dc8a18a
Michal Kolacek <kolacek@cisco.com>
2021-12-07 17:54:21 +0100

    Remove comments about Slido setup

--- dbt_superset_lineage/__init__.py
+++ dbt_superset_lineage/__init__.py
@@ -7,12 +7,11 @@ app = typer.Typer()
 @app.command()
 def docs_from_superset(dbt_project_dir: str = typer.Option('.', help=""),
                        exposures_path: str = typer.Option('/models/exposures/superset_dashboards.yml',
-                                                          # '/models/exposures/superset_dashboards.yml'
                                                           help="If you change this, the path needs to be added"
                                                                "to source-paths in dbt_project.yml."),
                        dbt_db_name: str = typer.Option(None, help=""),
-                       superset_url: str = typer.Argument(..., help=""),  # superset.sli.do
-                       superset_db_id: int = typer.Option(None, help=""),  # 2
+                       superset_url: str = typer.Argument(..., help=""),
+                       superset_db_id: int = typer.Option(None, help=""),
                        sql_dialect: str = typer.Option('ansi', help=""),
                        superset_access_token: str = typer.Option(None, envvar="SUPERSET_ACCESS_TOKEN"),
                        superset_refresh_token: str = typer.Option(None, envvar="SUPERSET_REFRESH_TOKEN")):
