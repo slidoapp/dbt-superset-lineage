f4cd9271c3347c0289bdc44a8f731d56ea3305ba
Michal Kolacek <kolacek@cisco.com>
2021-12-07 19:24:10 +0100

    Rename command from `docs_from_superset` to `pull_dashboards`

--- dbt_superset_lineage/__init__.py
+++ dbt_superset_lineage/__init__.py
@@ -1,24 +1,24 @@
 import typer
-from . import from_superset
+from .pull_dashboards import main as pull_dashboards_main
 
 app = typer.Typer()
 
 
 @app.command()
-def docs_from_superset(dbt_project_dir: str = typer.Option('.', help=""),
-                       exposures_path: str = typer.Option('/models/exposures/superset_dashboards.yml',
+def pull_dashboards(dbt_project_dir: str = typer.Option('.', help=""),
+                    exposures_path: str = typer.Option('/models/exposures/superset_dashboards.yml',
                                                           help="If you change this, the path needs to be added"
                                                                "to source-paths in dbt_project.yml."),
-                       dbt_db_name: str = typer.Option(None, help=""),
-                       superset_url: str = typer.Argument(..., help=""),
-                       superset_db_id: int = typer.Option(None, help=""),
-                       sql_dialect: str = typer.Option('ansi', help=""),
-                       superset_access_token: str = typer.Option(None, envvar="SUPERSET_ACCESS_TOKEN"),
-                       superset_refresh_token: str = typer.Option(None, envvar="SUPERSET_REFRESH_TOKEN")):
+                    dbt_db_name: str = typer.Option(None, help=""),
+                    superset_url: str = typer.Argument(..., help=""),
+                    superset_db_id: int = typer.Option(None, help=""),
+                    sql_dialect: str = typer.Option('ansi', help=""),
+                    superset_access_token: str = typer.Option(None, envvar="SUPERSET_ACCESS_TOKEN"),
+                    superset_refresh_token: str = typer.Option(None, envvar="SUPERSET_REFRESH_TOKEN")):
 
-    from_superset.main(dbt_project_dir, exposures_path, dbt_db_name,
-                       superset_url, superset_db_id, sql_dialect,
-                       superset_access_token, superset_refresh_token)
+    pull_dashboards_main(dbt_project_dir, exposures_path, dbt_db_name,
+                         superset_url, superset_db_id, sql_dialect,
+                         superset_access_token, superset_refresh_token)
 
 
 if __name__ == '__main__':
