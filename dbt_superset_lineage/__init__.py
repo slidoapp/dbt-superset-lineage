e39fbe61d761f5164ef85be56c5b27646b30e635
Michal Kolacek <kolacek@cisco.com>
2021-12-09 07:47:55 +0100

    Add help strings to command arguments and options

--- dbt_superset_lineage/__init__.py
+++ dbt_superset_lineage/__init__.py
@@ -6,16 +6,25 @@ app = typer.Typer()
 
 
 @app.command()
-def pull_dashboards(dbt_project_dir: str = typer.Option('.', help=""),
+def pull_dashboards(dbt_project_dir: str = typer.Option('.', help="Directory path to dbt project."),
                     exposures_path: str = typer.Option('/models/exposures/superset_dashboards.yml',
-                                                       help="If you change this, the path needs to be added"
-                                                            "to source-paths in dbt_project.yml."),
-                    dbt_db_name: str = typer.Option(None, help=""),
-                    superset_url: str = typer.Argument(..., help=""),
-                    superset_db_id: int = typer.Option(None, help=""),
-                    sql_dialect: str = typer.Option('ansi', help=""),
-                    superset_access_token: str = typer.Option(None, envvar="SUPERSET_ACCESS_TOKEN"),
-                    superset_refresh_token: str = typer.Option(None, envvar="SUPERSET_REFRESH_TOKEN")):
+                                                       help="Where within PROJECT_DIR the exposure file should "
+                                                            "be stored. If you set this to go outside /models, it then"
+                                                            "needs to be added to source-paths in dbt_project.yml."),
+                    dbt_db_name: str = typer.Option(None, help="Name of your database within dbt towards which "
+                                                               "the pull could be reduced to run."),
+                    superset_url: str = typer.Argument(..., help="URL of your Superset, e.g. "
+                                                                 "https://mysuperset.mycompany.com"),
+                    superset_db_id: int = typer.Option(None, help="ID of your database within Superset towards which "
+                                                                  "the pull could be reduced to run."),
+                    sql_dialect: str = typer.Option('ansi', help="Database SQL dialect; used for parsing queries. "
+                                                                 "Consult docs of SQLFluff for details: "
+                                                                 "https://docs.sqlfluff.com/en/stable/dialects.html?"
+                                                                 "highlight=ansi#dialects-reference"),
+                    superset_access_token: str = typer.Option(None, envvar="SUPERSET_ACCESS_TOKEN",
+                                                              help="Access token to Superset API."),
+                    superset_refresh_token: str = typer.Option(None, envvar="SUPERSET_REFRESH_TOKEN",
+                                                               help="Refresh token to Superset API.")):
 
     pull_dashboards_main(dbt_project_dir, exposures_path, dbt_db_name,
                          superset_url, superset_db_id, sql_dialect,
@@ -23,13 +32,20 @@ def pull_dashboards(dbt_project_dir: str = typer.Option('.', help=""),
 
 
 @app.command()
-def push_descriptions(dbt_project_dir: str = typer.Option('.', help=""),
-                      dbt_db_name: str = typer.Option(None, help=""),
-                      superset_url: str = typer.Argument(..., help=""),
-                      superset_db_id: int = typer.Option(None, help=""),
-                      superset_refresh_columns: bool = typer.Option(False, help=""),
-                      superset_access_token: str = typer.Option(None, envvar="SUPERSET_ACCESS_TOKEN"),
-                      superset_refresh_token: str = typer.Option(None, envvar="SUPERSET_REFRESH_TOKEN")):
+def push_descriptions(dbt_project_dir: str = typer.Option('.', help="Directory path to dbt project."),
+                      dbt_db_name: str = typer.Option(None, help="Name of your database within dbt to which the script "
+                                                                 "could be reduced to run."),
+                      superset_url: str = typer.Argument(..., help="URL of your Superset, e.g. "
+                                                                   "https://mysuperset.mycompany.com"),
+                      superset_db_id: int = typer.Option(None, help="ID of your database within Superset towards which "
+                                                                    "the push could be reduced to run."),
+                      superset_refresh_columns: bool = typer.Option(False, help="Whether columns in Superset should be "
+                                                                                "refreshed from database during "
+                                                                                "the push."),
+                      superset_access_token: str = typer.Option(None, envvar="SUPERSET_ACCESS_TOKEN",
+                                                                help="Access token to Superset API."),
+                      superset_refresh_token: str = typer.Option(None, envvar="SUPERSET_REFRESH_TOKEN",
+                                                                 help="Refresh token to Superset API.")):
 
     push_descriptions_main(dbt_project_dir, dbt_db_name,
                            superset_url, superset_db_id, superset_refresh_columns,
