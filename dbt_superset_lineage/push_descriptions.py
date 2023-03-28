ee88bea86590f55f728b8172955fa0b42ff21140
Michal Kolacek <kolacek@cisco.com>
2023-03-28 16:21:09 +0200

    Simplify how columns are updated

--- dbt_superset_lineage/push_descriptions.py
+++ dbt_superset_lineage/push_descriptions.py
@@ -146,15 +146,13 @@ def merge_columns_info(dataset, tables):
     columns_new = []
     for sst_column in sst_columns:
 
-        # add the mandatory field
         column_name = sst_column['column_name']
-        column_new = {'column_name': column_name}
 
-        # get all fields apart from the ones that shouldn't be in the update payload and empty "is_active" (errors out)
-        for field in sst_column:
-            if field not in ["changed_on", "created_on", "type_generic", "is_active"] \
-                    or (field == 'is_active' and sst_column[field] is not None):
-                column_new[field] = sst_column[field]
+        # add the mandatory fields
+        column_new = {
+            'column_name': column_name,
+            'id': sst_column['id']
+        }
 
         # add description
         if column_name in dbt_columns \
