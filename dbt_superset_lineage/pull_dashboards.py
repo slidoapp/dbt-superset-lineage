139259acfb1bc05bc77a87f2b64c96bc11245ff6
Michal Koláček <57877739+one-data-cookie@users.noreply.github.com>
2023-04-03 16:18:12 +0200

    Apply suggestions from code review
    Co-authored-by: Andrej Švec <andy.swec@gmail.com>

--- dbt_superset_lineage/pull_dashboards.py
+++ dbt_superset_lineage/pull_dashboards.py
@@ -9,7 +9,7 @@ import sqlfluff
 
 from .superset_api import Superset
 
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelprefix)s %(message)s, level=logging.INFO)
 logging.getLogger('sqlfluff').setLevel(level=logging.WARNING)
 
 
